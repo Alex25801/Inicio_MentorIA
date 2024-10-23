7aa7ad8f35facc35678aad898ca281b7b3738d89
Alex25801 <lgmil25801@gmail.com>
2024-10-23 15:29:51 -0400

    23/10 3:20

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USUARIO\Desktop\Proyectos\luzbeidi\19 10 24\venv
+set VIRTUAL_ENV=C:\Users\USUARIO\Desktop\Proyectos\luzbeidi\23-10-2024\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
